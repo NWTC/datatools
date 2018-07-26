2584bc170f1c65864b9912da587295dd67cf94d8
Dries Allaerts <dries.allaerts@nrel.gov>
2018-07-26 09:13:07 -0600

    Add smoothing function to sourceHistory.py

--- SOWFA/postProcessing/sourceHistory.py
+++ SOWFA/postProcessing/sourceHistory.py
@@ -151,7 +151,6 @@ class SourceHistory(object):
                     Nlines.append(i+1)
                     line = line.split() # check final line for the right number of values
                     if not len(line) == len(self.hLevelsCell)+2: # t,dt,f_1,f_2,...,f_N for N heights
-                        print('z',z)
                         print('line',line)
                         print('Error: number of output points inconsistent with hLevelsCell in', output)
                         return
@@ -190,3 +189,102 @@ class SourceHistory(object):
             for field in fields_to_check:
                 setattr(self, field, getattr(self,field)[:Nt_new,:])
             self.imax = Nt_new
+    
+    #==========================================================================
+    #
+    # CALCULATIONS
+    #
+    #==========================================================================
+
+    def smooth_sources(self,
+                     heights=[],
+                     tavg_window=600.0,
+                     dt=1.0,
+                     verbose=True):
+        """Smooth the source terms in time
+
+        Calculate smooth source terms at all heights when no specific height is asked for
+
+        INPUTS
+            tavg_window     size of window for moving average [s]
+            heights         vertical locations at which to calculate smooth source term [m]
+            dt              uniform time interval to which to interpolate [s]
+
+        CALCULATED QUANTITIES
+            tavg            uniformly spaced times at which a moving average was calculated
+            UXAvg           smoothed UX source term, *Avg.shape == ( len(tavg), len(heights) )
+            UYAvg           smoothed UY source term
+            UZAvg           smoothed UZ source term
+            TAvg            smoothed T  source term
+        """
+        try:
+            from scipy.ndimage import uniform_filter
+        except ImportError:
+            print('Moving average calculation uses scipy.ndimage')
+            return
+
+        if len(heights)==0:
+            heights = self.hLevelsCell
+        Nout  = len(heights)
+
+        self._trim_series_if_needed()
+
+        # setup uniform points for interpolation and averaging windows
+        Nt = int(np.ceil((self.t[-1]-self.t[0])/dt))
+        tuniform = np.arange(1,Nt+1)*dt + self.t[0]
+        Navg    = int(tavg_window/dt)
+        Navg_2  = int(Navg/2)
+        tavg    = tuniform[Navg_2:-Navg_2+1]
+        Ntavg   = len(tavg)
+        if verbose:
+            print('Interpolating to',Nt,'uniformly-spaced data points')
+            print('Moving average window:',tavg_window,'s')
+
+        UXAvg = np.zeros((Ntavg,Nout))
+        UYAvg = np.zeros((Ntavg,Nout))
+        UZAvg = np.zeros((Ntavg,Nout))
+        TAvg  = np.zeros((Ntavg,Nout))
+
+        for ih,z in enumerate(heights):
+            if not self.hLevelsCell.size == 1:
+                # setup interpolation
+                idx = np.nonzero(self.hLevelsCell > heights[ih])[0]
+                if len(idx)==0: # requested height too high
+                    k = len(self.hLevelsCell)-1 # extrapolate from two highest points
+                elif idx[0]==0: # request height too low
+                    k = 1 # extrapolate from two lowest points
+                else:
+                    k = idx[0] # interpolate between k-1, k
+                frac = (heights[ih] - self.hLevelsCell[k-1]) / (self.hLevelsCell[k] - self.hLevelsCell[k-1])
+    
+                # calculate time-averaged velocity profiles
+                # 1. interpolate to requested height for all times
+                # 2. interpolate from all times to a time history with uniformly-spaced samples
+                # 3. apply uniform_filter to perform moving average over the uniformly-spaced samples
+                UX_interp = self.UX[:,k-1] + frac*(self.UX[:,k] - self.UX[:,k-1]) # length=len(self.t)
+                UY_interp = self.UY[:,k-1] + frac*(self.UY[:,k] - self.UY[:,k-1])
+                UZ_interp = self.UZ[:,k-1] + frac*(self.UZ[:,k] - self.UZ[:,k-1])
+                T_interp  = self.T[:,k-1]  + frac*(self.T[:,k]  - self.T[:,k-1])
+            else:
+                UX_interp = self.UX.squeeze()
+                UY_interp = self.UY.squeeze()
+                UZ_interp = self.UZ.squeeze()
+                T_interp  = self.T.squeeze()
+            UX_uniform = np.interp( tuniform, self.t, UX_interp ) # length=Nt
+            UY_uniform = np.interp( tuniform, self.t, UY_interp )
+            UZ_uniform = np.interp( tuniform, self.t, UZ_interp )
+            T_uniform  = np.interp( tuniform, self.t, T_interp  )
+            UXAvg[:,ih] = uniform_filter( UX_uniform, Navg )[Navg_2:-Navg_2+1] # length=Ntavg
+            UYAvg[:,ih] = uniform_filter( UY_uniform, Navg )[Navg_2:-Navg_2+1]
+            UZAvg[:,ih] = uniform_filter( UZ_uniform, Navg )[Navg_2:-Navg_2+1]
+            TAvg[:,ih]  = uniform_filter( T_uniform,  Navg )[Navg_2:-Navg_2+1]
+
+        # end loop over heights
+
+        # save attributes, shape==(Ntavg,Nout)
+        self.Avg_heights = heights
+        self.tavg       = tavg
+        self.UXAvg      = UXAvg
+        self.UYAvg      = UYAvg
+        self.UZAvg      = UZAvg
+        self.TAvg       = TAvg
