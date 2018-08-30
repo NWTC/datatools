bfa2b891aec438511ee5132fc11206265af74ee2
Eliot Quon <eliot.quon@nrel.gov>
2018-08-30 00:33:15 -0600

    Clean up code
    Note: Unless there's a very specific way that the data are intended to
    be processed, it seems to make sense to just load all the data into
    memory at once (with a call to .values), otherwise using xarray could
    actually end up being slower if the same data segments are reloaded
    multiple times. This is the same thing that happens when the data are
    loaded with netCDF4.MFDataset (and referenced with [:]).

--- WRF/solution.py
+++ WRF/solution.py
@@ -26,50 +26,12 @@ class WRFSolution(object):
     def __init__(self,*args,**kwargs):
         verbose = kwargs.get('verbose',True)
         aggdim = kwargs.get('aggdim',default_aggdim)
-        use_xarray = kwargs.get('use_xarray',have_xarray)
-        if use_xarray:
+        self.use_xarray = kwargs.get('use_xarray',have_xarray)
+        if self.use_xarray:
             desc = 'with xarray'
         else:
             desc = 'with netcdf'
-        nclist = []
         Nfiles = len(args)
-#        self.filelist = [ fpath for fpath in args if os.path.isfile(fpath) ]
-#        for i,fpath in enumerate(args):
-#            try:
-#                if use_xarray:
-#                    nc = xarray.open_dataset(fpath)
-#                else:
-#                    nc = Dataset(fpath)
-#            except (IOError,OSError): # NetCDF: Unknown file format
-#                if verbose:
-#                    print('Skipped {:s} ({:d}/{:d})'.format(fpath,i+1,Nfiles))
-#                continue
-#            nclist.append(nc)
-#            if verbose:
-#                print('Loaded {:s} {:s} ({:d}/{:d})'.format(fpath,desc,i+1,Nfiles))
-#
-#        if len(nclist) == 0:
-#            raise IOError('No WRF solution files found!')
-#
-#        self.varlist = list(nc.variables)
-#        if verbose:
-#            print('  variables : ',self.varlist)
-#
-#        # dimensions (unstaggered, i.e., face/cell-centered)
-#        if use_xarray:
-#            self.Nt, self.Nz, self.Ny, self.Nx = nc.variables['U'].shape
-#            self.Nx -= 1 # U is staggered in x
-#        else:
-#            self.Nt = nc.dimensions['time'].size
-#            self.Nx = nc.dimensions['west_east'].size 
-#            self.Ny = nc.dimensions['south_north'].size
-#            self.Nz = nc.dimensions['bottom_top'].size
-#        if verbose:
-#            print('  dimensions : ',self.Nt,self.Nx,self.Ny,self.Nz)
-#
-#        # read variables and unstagger
-#        self._read_vars(nclist)
-
         self.filelist = []
         for fpath in [ fpath for fpath in args if os.path.isfile(fpath) ]:
             try:
@@ -78,7 +40,7 @@ class WRFSolution(object):
                 pass
             else:
                 self.filelist.append(fpath)
-        if use_xarray:
+        if self.use_xarray:
             nc = xarray.open_mfdataset(self.filelist, concat_dim=aggdim)
             self.Nt, self.Nz, self.Ny, self.Nx = nc.variables['U'].shape
             self.Nx -= 1 # U is staggered in x
@@ -89,9 +51,9 @@ class WRFSolution(object):
             self.Ny = len(nc.dimensions['south_north'])
             self.Nz = len(nc.dimensions['bottom_top'])
         self.varlist = list(nc.variables)
-        self._read_vars(nc,use_xarray)
+        self._read_vars(nc)
 
-    def _read_vars(self,nc,use_xarray=False):
+    def _read_vars(self,nc):
         # unstaggered
         self.T = nc.variables['T'][:] + 300.0
         # staggered in x
@@ -117,58 +79,14 @@ class WRFSolution(object):
                 for j in range(self.Ny):
                     self.z[:,i,j,:] -= hgt[i,j]
 
-        # if using xarray, arrays still have yet to be accessed
-        if use_xarray:
-            # preload all the values into memory
+        # xarray doesn't read in the mfdataset until we call .values
+        if self.use_xarray:
             self.z = self.z.values
             self.U = self.U.values
             self.V = self.V.values
             self.W = self.W.values
             self.T = self.T.values
 
-#    def _read_vars(self,nclist):
-#        """DEPRECATED: Read all useful variables, shape==(NT,NZ,NY,NX)"""
-#        zlist = []
-#        Tlist = []
-#        Ulist = []
-#        Vlist = []
-#        Wlist = []
-#        for i,nc in enumerate(nclist):
-#            sys.stderr.write('\rReading vars from {:s}'.format(self.filelist[i]))
-#            Tlist.append( nc.variables['T'][:] )
-#            # staggered in x
-#            U = nc.variables['U'][:]
-#            Ulist.append( 0.5*(U[:,:,:,:-1] + U[:,:,:,1:]) )
-#            # staggered in y
-#            V = nc.variables['V'][:]
-#            Vlist.append( 0.5*(V[:,:,:-1,:] + V[:,:,1:,:]) )
-#            # staggered in z
-#            W = nc.variables['W'][:]
-#            PH = nc.variables['PH'][:]
-#            PHB = nc.variables['PHB'][:]
-#            Wlist.append( 0.5*(W[:,:-1,:,:] + W[:,1:,:,:]) )
-#            # calculate z == (ph + phb)/g
-#            zlist.append( 0.5*( PH[:,:-1,:,:] +  PH[:,1:,:,:] +
-#                               PHB[:,:-1,:,:] + PHB[:,1:,:,:] ) / g )
-#        sys.stderr.write('\n')
-#
-#        print('Concatenating data arrays')
-#        self.U = np.concatenate(Ulist, axis=0)
-#        self.V = np.concatenate(Vlist, axis=0)
-#        self.W = np.concatenate(Wlist, axis=0)
-#        self.T = np.concatenate(Tlist, axis=0) + 300.0
-#
-#        # calculate height AGL
-#        print('Calculating height AGL')
-#        self.z = np.concatenate(zlist, axis=0)
-#        if 'HGT' in self.varlist:
-#            # TODO: test this
-#            hgt = nclist[0].variables['HGT'][:]
-#            for i in range(self.Nx):
-#                for j in range(self.Ny):
-#                    self.z[:,i,j,:] -= hgt[i,j]
-
-    
     def sample_profile(self,itime=slice(0,None),i=None,j=None,overwrite=False):
         """Extracts velocity and temperature profile at a specified
         location (defaults to center of domain).
@@ -180,20 +98,25 @@ class WRFSolution(object):
             i = int(self.Nx / 2)
         if j is None:
             j = int(self.Ny / 2)
+        zprofile = self.z[itime,:,j,i]
+        Uprofile = self.U[itime,:,j,i]
+        Vprofile = self.V[itime,:,j,i]
+        Wprofile = self.W[itime,:,j,i]
+        Tprofile = self.T[itime,:,j,i]
         if overwrite:
-            self.z = self.z[itime,:,j,i]
-            self.U = self.U[itime,:,j,i]
-            self.V = self.V[itime,:,j,i]
-            self.W = self.W[itime,:,j,i]
-            self.T = self.T[itime,:,j,i]
+            self.z = zprofile
+            self.U = Uprofile
+            self.V = Vprofile
+            self.W = Wprofile
+            self.T = Tprofile
         else:
-            return {
-                'z':self.z[itime,:,j,i],
-                'U':self.U[itime,:,j,i],
-                'V':self.V[itime,:,j,i],
-                'W':self.W[itime,:,j,i],
-                'T':self.T[itime,:,j,i]
-            }
+            return dict(
+                z=zprofile,
+                U=Uprofile,
+                V=Vprofile,
+                W=Wprofile,
+                T=Tprofile
+            )
 
     def approx_z(self):
         self.zmean = self.z.mean(axis=(0,2,3))
