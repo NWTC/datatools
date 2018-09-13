a93078f8e992bfdba143a359760b71c42023033f
Eliot Quon <eliot.quon@nrel.gov>
2018-09-13 13:41:39 -0600

    Explicitly specify plot time to avoid race condition
    so that sowfa_averaging_plotall.py may be used while the simulation is
    still running

--- utilities/sowfa_averaging_plotall.py
+++ utilities/sowfa_averaging_plotall.py
@@ -25,6 +25,7 @@ wind_aligned = True
 # reads avg.hLevelsCell with shape (NZ)
 # reads avg.U_mean, ... with shape (NT, NZ)
 avg = PlanarAverages( *sys.argv[1:] )
+tmax = avg.t[-1]
 
 #------------------------------------------------------------------------------
 #
@@ -54,15 +55,15 @@ for i,h in enumerate(heights):
 #
 if wind_aligned:
     fig,ax = plt.subplots(ncols=3)
-    avg.plot_windspeed_profile(ax=ax[0])
-    avg.plot_winddirection_profile(ax=ax[1])
-    avg.plot_T_profile(ax=ax[2])
+    avg.plot_windspeed_profile(time=tmax,ax=ax[0])
+    avg.plot_winddirection_profile(time=tmax,ax=ax[1])
+    avg.plot_T_profile(time=tmax,ax=ax[2])
     ax[1].set_ylabel('')
     ax[2].set_ylabel('')
 else:
     fig,ax = plt.subplots(ncols=2)
-    avg.plot_UVW_profile(ax=ax[0])
-    avg.plot_T_profile(ax=ax[1])
+    avg.plot_UVW_profile(time=tmax,ax=ax[0])
+    avg.plot_T_profile(time=tmax,ax=ax[1])
     ax[1].set_ylabel('')
 fig.savefig('Profiles_Mean.png',bbox_inches='tight')
 fig.suptitle('Resolved Mean Quantities')
@@ -72,8 +73,8 @@ fig.suptitle('Resolved Mean Quantities')
 # Resolved Fluctuating Quantities
 #
 fig,ax = plt.subplots(ncols=2)
-avg.plot_variance_profile(ax=ax[0],rotated=wind_aligned)
-avg.plot_covariance_profile(ax=ax[1],rotated=wind_aligned)
+avg.plot_variance_profile(time=tmax,ax=ax[0],rotated=wind_aligned)
+avg.plot_covariance_profile(time=tmax,ax=ax[1],rotated=wind_aligned)
 ax[1].set_ylabel('')
 fig.savefig('Profiles_Fluc.png',bbox_inches='tight')
 fig.suptitle('Resolved Fluctuating Quantities')
@@ -83,8 +84,8 @@ fig.suptitle('Resolved Fluctuating Quantities')
 # Modeled SFS Quantities
 #
 fig,ax = plt.subplots(ncols=2)
-avg.plot_SFS_normalstress_profile(ax=ax[0],rotated=wind_aligned)
-avg.plot_SFS_shearstress_profile(ax=ax[1],rotated=wind_aligned)
+avg.plot_SFS_normalstress_profile(time=tmax,ax=ax[0],rotated=wind_aligned)
+avg.plot_SFS_shearstress_profile(time=tmax,ax=ax[1],rotated=wind_aligned)
 ax[1].set_ylabel('')
 fig.savefig('Profiles_SFS.png',bbox_inches='tight')
 fig.suptitle('Sub-Filter Scale Quantities')
