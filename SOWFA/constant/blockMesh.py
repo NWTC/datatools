a63da829e2926acf13882554b0b46e07d1ea948b
Eliot Quon <eliot.quon@nrel.gov>
2018-10-19 00:20:35 -0600

    Implement blockMeshDict file output functions

--- SOWFA/constant/blockMesh.py
+++ SOWFA/constant/blockMesh.py
@@ -76,3 +76,264 @@ def estimate(d, d0, r, L=None, round_to=10., output='blockMesh'):
     else:
         return L, N, r_approx**(N-1)
 
+
+blockMeshDict_header = """/*--------------------------------*- C++ -*----------------------------------*\\
+| =========                 |                                                 |
+| \\\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
+|  \\\\    /   O peration     | Version:  1.6                                   |
+|   \\\\  /    A nd           | Web:      http://www.OpenFOAM.org               |
+|    \\\\/     M anipulation  |                                                 |
+\*---------------------------------------------------------------------------*/
+FoamFile
+{{
+    version     2.0;
+    format      ascii;
+    class       dictionary;
+    object      blockMeshDict;
+}}
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include        "../../setUp"
+
+convertToMeters 1.0;
+
+"""
+
+patch_def = """    {name:s}
+    {{
+        type patch;
+        faces
+        (
+{faceslist:s}
+        );
+    }}
+"""
+
+blockMeshDict_footer = """
+// ************************************************************************* //"""
+
+
+class BlockMeshDict(object):
+    """Used to manage mesh regions within a blockMesh definition. At the
+    moment, only vertically layered regions are supported.
+    """
+
+    def __init__(self,xMin,xMax,yMin,yMax,zMin,zMax,dx,dy,dz): 
+        """Initialize domain with specified overall dimensions
+        (Lx,Ly,Lz) with finest-level grid spacings (dx,dy,dz). Actual
+        domain extents will be greater than or equal to the inputs,
+        depending on whether the input spacings exactly fill up the
+        domain.
+        """
+        self.xMin = xMin
+        self.yMin = yMin
+        self.zMin = zMin
+        self.xMax = xMax
+        self.yMax = yMax
+        self.zMax = zMax
+        self.dx0 = dx
+        self.dy0 = dy
+        self.dz0 = dz
+
+        self.vertex0 = 0
+        self.blocks = []
+
+        # descriptors of each region
+        self.Nx = []
+        self.Ny = []
+        self.Nz = []
+        self.z0 = []
+        self.z1 = []
+        self.simpleGradingZ = []
+
+    def __repr__(self):
+        Nlayers = len(self.Nx)
+        s = 'Mesh bounding box corners: ({:g} {:g} {:g}) ({:g} {:g} {:g})\n'.format(
+                self.xMin, self.yMin, self.zMin,
+                self.xMax, self.yMax, self.zMax)
+        if Nlayers == 0:
+            s += '  no layers added; call generate_uniform_grid or generate_region'
+        for i in range(Nlayers):
+            dx = (self.xMax - self.xMin) / self.Nx[i]
+            dy = (self.yMax - self.yMin) / self.Ny[i]
+            dz = (self.z1[i] - self.z0[i]) / self.Nz[i]
+            s+= '  layer {:d} : z=({:g} {:g}) N=({:d} {:d} {:d}) spacings=({:g} {:g} {:g})\n'.format(
+                    i, self.z0[i], self.z1[i],
+                    self.Nx[i], self.Ny[i], self.Nz[i],
+                    dx, dy, dz)
+        return s
+            
+
+    def generate_uniform_grid(self):
+        self.generate_region(self.zMin, self.zMax, self.dx0, self.dy0, self.dz0,
+                             add=True)
+
+    def generate_region(self,z0,z1,dx,dy,dz0,dz1=None,r=1,add=False):
+        """Generate new region between height z0 and z1, with grid
+        spacings dx, dy, and dz.
+
+        Optionally, a growth rate r > 1 may be specified, in which case
+        the target final grid spacing (dz1) should be specified.
+
+        The new region is added to the domain until the add=True is
+        specified.
+        """
+        Nx = int(np.ceil((self.xMax-self.xMin)/dx))
+        x0 = self.xMin
+        x1 = x0 + Nx*dx
+        if x1 > self.xMax:
+            self.xMax = x1
+            print('Updated xMax to',self.xMax)
+        Ny = int(np.ceil((self.yMax-self.yMin)/dy))
+        y0 = self.yMin
+        y1 = y0 + Ny*dy
+        if y1 > self.yMax:
+            self.yMax = y1
+            print('Updated yMax to',self.yMax)
+
+        if r==1:
+            Nz = int(np.ceil((z1-z0)/dz0))
+            ratio = 1.0
+        else:
+            assert(dz1 is not None)
+            Lz,Nz,ratio = estimate(dz1,dz0,r,L=(z1-z0))
+            assert(Lz == z1-z0)
+        
+        if add:
+            self.Nx.append(Nx)
+            self.Ny.append(Ny)
+            self.Nz.append(Nz)
+            self.z0.append(z0)
+            self.z1.append(z1)
+            self.simpleGradingZ.append(ratio)
+            hexblock = hex(self.vertex0, N=(Nx,Ny,Nz),
+                           simpleGrading=(1,1,ratio))
+            self.blocks.append(hexblock)
+            self.vertex0 += 8
+            print('Added region',len(self.Nx))
+
+    def _vertices(self):
+        s = 'vertices\n('
+        for i in range(self.Nlayers):
+            s += """
+    ( $xMin   $yMin   $zMin{i:d} )
+    ( $xMax   $yMin   $zMin{i:d} )
+    ( $xMax   $yMax   $zMin{i:d} )
+    ( $xMin   $yMax   $zMin{i:d} )
+    ( $xMin   $yMin   $zMax{i:d} )
+    ( $xMax   $yMin   $zMax{i:d} )
+    ( $xMax   $yMax   $zMax{i:d} )
+    ( $xMin   $yMax   $zMax{i:d} )""".format(i=i)
+        s += '\n);\n\n'
+        return s
+
+    def _blocks(self):
+        s = 'blocks\n(\n'
+        for block in self.blocks:
+            s += '    ' + block.write() + '\n'
+        s += ');\n\n'
+        return s
+
+    def _edges(self):
+        return 'edges\n(\n);\n\n'
+
+    def _boundary(self):
+        s = 'boundary\n(\n'
+        # write out interfaces
+        self.patch_pairs = []
+        for i in range(self.Nlayers-1):
+            name1 = 'interface{:d}{:d}'.format(i+1,i+2)
+            name2 = 'interface{:d}{:d}'.format(i+2,i+1)
+            self.patch_pairs.append((name1,name2))
+            s += patch_def.format(name=name1,
+                                  faceslist='            '+self.blocks[i].top())
+            s += patch_def.format(name=name2,
+                                  faceslist='            '+self.blocks[i].bottom())
+        # lower boundary
+        lowerfaces = '            '+self.blocks[0].lower()
+        s += patch_def.format(name='lower',faceslist=lowerfaces)
+        # upper boundary
+        upperfaces = '            '+self.blocks[-1].upper()
+        s += patch_def.format(name='upper',faceslist=upperfaces)
+        # west boundary
+        westfaces = '\n'.join([ '            '+self.blocks[i].west()
+                                for i in range(self.Nlayers) ])
+        s += patch_def.format(name='west',faceslist=westfaces)
+        # east boundary
+        eastfaces = '\n'.join([ '            '+self.blocks[i].east()
+                                for i in range(self.Nlayers) ])
+        s += patch_def.format(name='east',faceslist=eastfaces)
+        # north boundary
+        northfaces = '\n'.join([ '            '+self.blocks[i].north()
+                                 for i in range(self.Nlayers) ])
+        s += patch_def.format(name='north',faceslist=northfaces)
+        # south boundary
+        southfaces = '\n'.join([ '            '+self.blocks[i].south()
+                                 for i in range(self.Nlayers) ])
+        s += patch_def.format(name='south',faceslist=southfaces)
+        s += ');\n\n'
+        return s
+
+    def _mergedPatchPairs(self):
+        s = 'mergedPatchPairs\n(\n'
+        for pair in self.patch_pairs:
+            s += '    ' + str(pair) + '\n'
+        s += ');\n'
+        return s
+
+    def write(self,fpath='blockMeshDict'):
+        """Write out blockMeshDict, which should be placed into
+        constant/polyMesh.
+        """
+        assert(len(self.Nx) == len(self.Ny) == len(self.Nz) ==
+               len(self.z0) == len(self.z1) == len(self.simpleGradingZ))
+        self.Nlayers = len(self.Nx)
+        with open(fpath,'w') as f:
+            f.write(blockMeshDict_header)
+            f.write(self._vertices())
+            f.write(self._blocks())
+            f.write(self._edges())
+            f.write(self._boundary())
+            f.write(self._mergedPatchPairs())
+            f.write(blockMeshDict_footer)
+        print('Wrote '+fpath)
+
+
+class hex(object):
+    """Helper class for managing hex blocks"""
+
+    def __init__(self, vertex0=0, N=(0,0,0), grading='simpleGrading', simpleGrading=(1,1,1)):
+        self.vertices = np.arange(vertex0,vertex0+8)
+        self.Ncells = N
+        self.grading = grading
+        self.simpleGrading = simpleGrading
+
+    def write(self):
+        return 'hex ({:s}) ({:s}) {:s} ({:s})'.format(
+                ' '.join(['{:d}'.format(val) for val in self.vertices]),
+                ' '.join(['{:d}'.format(val) for val in self.Ncells]),
+                self.grading,
+                ' '.join(['{:g}'.format(val) for val in self.simpleGrading])
+                )
+
+    def _facelist(self,v):
+        return '('+' '.join([str(i) for i in v])+')'
+
+    def west(self):
+        return self._facelist(self.vertices[[0,4,7,3]])
+
+    def east(self):
+        return self._facelist(self.vertices[[1,2,6,5]])
+
+    def south(self):
+        return self._facelist(self.vertices[[0,1,5,4]])
+
+    def north(self):
+        return self._facelist(self.vertices[[2,3,7,6]])
+
+    def lower(self):
+        return self._facelist(self.vertices[[0,3,2,1]])
+
+    def upper(self):
+        return self._facelist(self.vertices[[4,5,6,7]])
+
