d59c84ba83c65ca3b2f5ccd50d7b10a0e804687f
Eliot Quon <eliot.quon@nrel.gov>
2018-10-24 17:56:55 -0600

    Add background_box source
    Add 'turbine_' prefix to the box and cylinder sources

--- SOWFA/system/topoSet.py
+++ SOWFA/system/topoSet.py
@@ -31,7 +31,7 @@ topoSetDict_footer = """);
 
 
 class TopoSetDict(object):
-    source_types = ['box','cylinder']
+    source_types = ['background_box','turbine_box','turbine_cylinder']
 
     def __init__(self,sources=[],perturb=0.01,rotation=0.0,
                  upstream=5.0,downstream=10.0,width=3.0,height=3.0,
@@ -40,6 +40,10 @@ class TopoSetDict(object):
         for refinement). Level 1 is the finest level, and successive
         refinements should be performed sequentially starting from the
         coarsest level down to 1.
+
+        Note that defaults are only specified for turbine refinement
+        regions; the actual dimensional values must be specified for 
+        each background refinement region.
         
         Inputs
         ------
@@ -51,8 +55,8 @@ class TopoSetDict(object):
             A perturbation of the refinement boxes to keep the
             boundaries off of cell faces. [m]
 
-        Default box refinement parameters
-        ---------------------------------
+        Default turbine box refinement parameters
+        -----------------------------------------
         rotation : float, optional
             Angle about the z-axis to rotate the refinement region (NOT
             the compass wind direction); if None, then mean_rotation is
@@ -95,6 +99,16 @@ class TopoSetDict(object):
             vertical_buffer=vertical_buffer
         )
 
+        # definitions for each background region
+        self.LLcorner = []
+        self.bkg_rotation = []
+        self.bkg_length = []
+        self.bkg_width = []
+        self.bkg_height = []
+        self.bkg_xbuffer = []
+        self.bkg_ybuffer = []
+        self.bkg_zbuffer = []
+
         # definitions for each turbine
         self.base_location = []
         self.rotation = []
@@ -121,6 +135,49 @@ class TopoSetDict(object):
         defined = [ (sourcename in self.source_types)
                     for sourcename in self.sources ]
         assert(all(defined))
+
+    def add_background(self, LLcorner=(0,0,0),
+            length=0.0, width=0.0, height=0.0, rotation=0.0,
+            streamwise_buffer=50.0, lateral_buffer=50.0, vertical_buffer=50.0):
+        """Add refinement box at location specified by the lower-left
+        corner 'LLcorner' with dimensions given by Lx, Ly, Lz.
+        
+        By default, the box is aligned with the x-, y-, and z- axes.
+
+        Background box parameters
+        -------------------------
+        LLcorner : array-like
+            xyz coordinates of lower-left corner of the refinement box
+            [m]
+        length : float
+            Length of box in the x-direction. [m]
+        width : float
+            Width of box in the y-direction. [m]
+        height : float
+            Height of box in the x-direction. [m]
+        rotation : float, optional
+            Angle about the z-axis to rotate the refinement region. [deg]
+        streamwise_buffer : float, optional
+            Size of buffer region between refinement levels in the upstream
+            and downstream directions. [m]
+        lateral_buffer : float, optional
+            Size of buffer region between refinement levels in the lateral
+            directions. [m]
+        vertical_buffer : float, optional
+            Size of buffer region between refinement levels in the vertical
+            direction. [m]
+        """
+        assert((length > 0) and (width > 0) and (height > 0))
+        self.LLcorner.append(LLcorner)
+        if 'rotation' is None:
+            rotation = self.refinement['rotation']
+        self.bkg_rotation.append(rotation)
+        self.bkg_length.append(length)
+        self.bkg_width.append(width)
+        self.bkg_height.append(height)
+        self.bkg_xbuffer.append(streamwise_buffer)
+        self.bkg_ybuffer.append(lateral_buffer)
+        self.bkg_zbuffer.append(vertical_buffer)
 
     def add_turbine(self, base_location=(1000,1000,0), D=126.0, **kwargs):
         """Add turbine at specified 'base_location' with diameter 'D'.
@@ -176,13 +233,68 @@ class TopoSetDict(object):
             # write out topoSetDict.*
             with open(fname,'w') as f:
                 f.write(topoSetDict_header.format(fname=fname))
-                for iturb in range(len(self.base_location)):
-                    #f.write(source(iturb,ilevel)) # ilevel is 0-indexed
-                    f.write(source(iturb,efflevel))
+                if sourcename.startswith('turbine'):
+                    for iturb in range(len(self.base_location)):
+                        f.write(source(iturb,efflevel)) # levels are 0-indexed
+                elif sourcename.startswith('background'):
+                    for ibkg in range(len(self.LLcorner)):
+                        f.write(source(ibkg,efflevel)) # levels are 0-indexed
+                else:
+                    print("Unrecognized source type for source '{:s}'".format(sourcename))
                 f.write(topoSetDict_footer)
 
 
-    def _write_box(self,iturb,ilevel):
+    def _write_background_box(self,ibkg,ilevel):
+        template = """    {{
+        name         local;
+        type         cellSet;
+        action       {action:s};
+        source       rotatedBoxToCell;
+        sourceInfo
+        {{
+            origin ( {x0:g} {y0:g} {z0:g} );
+            i      ( {ix:g} {iy:g} {iz:g} );
+            j      ( {jx:g} {jy:g} {jz:g} );
+            k      ( {kx:g} {ky:g} {kz:g} );
+        }}
+    }}
+"""
+        if ibkg == 0:
+            action = 'new'
+        else:
+            action = 'add'
+        length = self.bkg_length[ibkg]
+        width = self.bkg_width[ibkg]
+        height = self.bkg_height[ibkg]
+        xbuff = self.bkg_xbuffer[ibkg]
+        ybuff = self.bkg_ybuffer[ibkg]
+        zbuff = self.bkg_zbuffer[ibkg]
+        LLcorner = self.LLcorner[ibkg]
+        ang = self.bkg_rotation[ibkg]
+        # origin (x,y,z)
+        x0 = -ilevel*xbuff
+        y0 = -ilevel*ybuff
+        x = x0 * np.cos(ang) - y0 * np.sin(ang) + LLcorner[0]
+        y = x0 * np.sin(ang) + y0 * np.cos(ang) + LLcorner[1]
+        z = base[2]
+        # box dimensions
+        ix = (length + 2*ilevel*xbuff) * np.cos(ang)
+        iy = (length + 2*ilevel*xbuff) * np.sin(ang)
+        iz = 0.0
+        jx = -(width + 2*ilevel*ybuff) * np.sin(ang)
+        jy =  (width + 2*ilevel*ybuff) * np.cos(ang)
+        jz = 0.0
+        kx = 0.0
+        ky = 0.0
+        kz = height + ilevel*zbuff
+        return template.format(action=action,
+                x0=x, y0=y, z0=z,
+                ix=ix, iy=iy, iz=iz,
+                jx=jx, jy=jy, jz=jz,
+                kx=kx, ky=ky, kz=kz)
+
+
+    def _write_turbine_box(self,iturb,ilevel):
         template = """    {{
         name         local;
         type         cellSet;
@@ -235,18 +347,14 @@ class TopoSetDict(object):
                 kx=kx, ky=ky, kz=kz)
 
 
-    def _write_cylinder(self,iturb,ilevel):
+    def _write_turbine_cylinder(self,iturb,ilevel):
         template = """    {{
         name         local;
         type         cellSet;
         action       {action:s};
-        source       rotatedBoxToCell;
+        source       ;
         sourceInfo
         {{
-            origin ( {x0:g} {y0:g} {z0:g} );
-            i      ( {ix:g} {iy:g} {iz:g} );
-            j      ( {jx:g} {jy:g} {jz:g} );
-            k      ( {kx:g} {ky:g} {kz:g} );
         }}
     }}
 """
