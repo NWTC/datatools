f508e5db76574d8f204c768a564cf2af7afe14bc
Eliot Quon <eliot.quon@nrel.gov>
2018-11-25 13:42:26 -0700

    Add module for generating turbineArrayProperties

---
+++ SOWFA/constant/turbineArrayProperties.py
@@ -0,0 +1,143 @@
+header = """/*--------------------------------*- C++ -*----------------------------------*\\
+| =========                 |                                                 |
+| \\\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
+|  \\\\    /   O peration     | Version:  2.0                                   |
+|   \\\\  /    A nd           | Web:      http://www.OpenFOAM.org               |
+|    \\\\/     M anipulation  |                                                 |
+\\*---------------------------------------------------------------------------*/
+FoamFile
+{{
+    version     2.0;
+    format      ascii;
+    class       dictionary;
+    object      turbineArrayProperties;
+}}
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+ 
+globalProperties
+{{
+    outputControl       {outputControl:s};
+    outputInterval      {outputInterval:g};
+}}
+"""
+
+turbine_definition = """{name:s}
+{{
+    turbineType                      "{turbineType:s}";
+    includeNacelle                    {includeNacelle:s};
+    includeTower                      {includeTower:s};
+    baseLocation                     ({baseLocation[0]:g} {baseLocation[1]:g} {baseLocation[2]:g});
+    numBladePoints                    {numBladePoints:d};
+    numNacellePoints                  {numNacellePoints:d};
+    numTowerPoints                    {numTowerPoints:d};
+    bladePointDistType               "{bladePointDistType:s}";  // (uniform | ...)
+    nacellePointDistType             "{nacellePointDistType:s}";  // (uniform | ...)
+    towerPointDistType               "{towerPointDistType:s}";  // (uniform | ...)
+    bladeSearchCellMethod            "{bladeSearchCellMethod:s}";  // (sphere | disk)
+    bladeActuatorPointInterpType     "integral";  // (integral | linear | cellCenter)
+    nacelleActuatorPointInterpType   "linear";  // (linear | cellCenter)
+    towerActuatorPointInterpType     "linear";  // (linear | cellCenter)
+    actuatorUpdateType               "{actuatorUpdateType:s}";  // (oldPosition | newPosition)
+    bladeForceProjectionType         "{bladeForceProjectionType:s}";  // (uniformGaussian | generalizedGaussian | generalizedGaussian2D | variableGaussianUserDef | variableUniformGaussianChord | chordThicknessGaussian | chordThicknessGaussian2D)
+    nacelleForceProjectionType       "{nacelleForceProjectionType:s}"; // (uniformGaussian | diskGaussian | advanced*)
+    towerForceProjectionType         "{towerForceProjectionType:s}";  // (uniformGaussian | diskGaussian | ringGaussian | advanced*)
+    bladeForceProjectionDirection    "{bladeForceProjectionDirection:s}";  // (localVelocityAligned | localVelocityAlignedCorrected | sampleVelocityAligned)
+    bladeEpsilon                     ({bladeEpsilon[0]:f} {bladeEpsilon[1]:f} {bladeEpsilon[2]:f});
+    nacelleEpsilon                   ({nacelleEpsilon[0]:f} {nacelleEpsilon[1]:f} {nacelleEpsilon[2]:f});
+    towerEpsilon                     ({towerEpsilon[0]:f} {towerEpsilon[1]:f} {towerEpsilon[2]:f});
+    nacelleSampleDistance             {nacelleSampleDistance:f};
+    towerSampleDistance               {towerSampleDistance:f};
+    tipRootLossCorrType              "{tipRootLossCorrType:s}";  // (none | Glauert)
+    rotationDir                      "{rotationDir:s}";  // (cw | ccw)
+    Azimuth                           {azimuth:f};
+    RotSpeed                          {rotorSpeed:f};
+    TorqueGen                         {torqueGen:f};
+    Pitch                             {pitch:f};
+    NacYaw                            {nacelleYaw:f};
+    fluidDensity                      {fluidDensity:f};
+}}
+"""
+
+defaults = dict(
+    includeNacelle='false',
+    includeTower='false',
+    numNacellePoints=10,
+    numTowerPoints=80,
+    bladePointDistType='uniform',
+    nacellePointDistType='uniform',
+    towerPointDistType='uniform',
+    bladeSearchCellMethod='disk',
+    bladeActuatorPointInterpType='integral',
+    nacelleActuatorPointInterpType='linear',
+    towerActuatorPointInterpType='linear',
+    actuatorUpdateType='oldPosition',
+    bladeForceProjectionType='uniformGaussian',
+    nacelleForceProjectionType='diskGaussian',
+    towerForceProjectionType='diskGaussian',
+    bladeForceProjectionDirection='localVelocityAligned',
+    nacelleEpsilon=(0,0,0),
+    towerEpsilon=(0,0,0),
+    nacelleSampleDistance=1.0,
+    towerSampleDistance=1.0,
+    tipRootLossCorrType='Glauert',
+    rotationDir='cw',
+    azimuth=0.0,
+    rotorSpeed=0.0,
+    torqueGen=0.0,
+    pitch=0.0,
+    fluidDensity=1.23,
+)
+
+
+class TurbineArrayProperties(object):
+    """Class for creating constant/turbineArrayProperties input files"""
+    def __init__(self,outputControl='timeStep',outputInterval=1,
+                 **kwargs):
+        """Optional turbine properties are specified as keyword
+        arguments. See turbineArrayProperties.defaults for default
+        parameters.
+        """
+        self.outputControl = outputControl;
+        self.outputInterval = outputInterval;
+        self.properties = defaults.copy()
+        self.turbdefs = []
+
+    def add_turbine(self,turbineType,
+                    baseLocation,numBladePoints,bladeEpsilon,nacelleYaw,
+                    **kwargs):
+        """Add turbine at 'baseLocation' with the specified number of
+        actuator points and spreading parameters (epsilon). 
+        'turbineType' should correspond to a turbine definition in
+        constant/turbineProperties/<turbineType>.
+
+        The turbine orientation is determined by 'nacelleYaw', specified
+        as a _compass_ direction in degrees.
+
+        Default turbine properties may be overridden using keyword
+        arguments.
+        """
+        d = self.properties.copy()
+        d['turbineType'] = turbineType
+        d['baseLocation'] = baseLocation
+        d['numBladePoints'] = numBladePoints
+        d['bladeEpsilon'] = bladeEpsilon
+        d['nacelleYaw'] = nacelleYaw
+        for key,val in kwargs.items():
+            if not key in d:
+                print("Warning: specified parameter '{:s}' not found")
+            elif isinstance(val,bool):
+                d[key] = str(val).lower()
+            else:
+                d[key] = val
+        self.turbdefs.append(d)
+
+    def write(self,fpath='turbineArrayProperties'):
+        """Write OpenFOAM dictionary constant/turbineArrayProperties"""
+        with open(fpath,'w') as f:
+            f.write(header.format(outputControl=self.outputControl,
+                                  outputInterval=self.outputInterval))
+            for iturb, d in enumerate(self.turbdefs):
+                d['name'] = 'turbine' + str(iturb)
+                f.write(turbine_definition.format(**d))
+        print('Wrote',fpath)
+
