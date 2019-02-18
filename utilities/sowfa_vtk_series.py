732d40661b03dc5fbd879a807b565d2743049b51
Eliot Quon <eliot.quon@nrel.gov>
2019-02-18 03:00:58 -0700

    Add optional argument to specify variable name(s)
    to detect from sampled file name--the remaining name after expected
    variables have been stripped is the sample name. Also, the '--nameFirst'
    argument has been removed; this is now automatically detected.

--- utilities/sowfa_vtk_series.py
+++ utilities/sowfa_vtk_series.py
@@ -18,23 +18,23 @@ import argparse
 
 # workarounds:
 extMapping = dict(xy='xyz')
-underscoreNames = ['p_rgh']
+expectedVars = ['U','T','k','p','p_rgh']
 
 # parse command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('srcdir',nargs='?',default=os.getcwd(),help='alternate source directory')
-parser.add_argument('--nameFirst',action='store_true',
-                    help='parse output files assuming the sampleName preceeds the variable name(s)')
+parser.add_argument('--vars',nargs='+',default=expectedVars,
+                    help='specify expected variable names')
 parser.add_argument('-v','--verbose',action='store_true',
                     help='verbose output (for debugging)')
 args = parser.parse_args()
 
 verbose = args.verbose
-srcdir = args.srcdir
 
 # find time subdirectories
 dirlist = []
 timesteps = []
+srcdir = args.srcdir
 dirs = os.walk(srcdir).next()[1]
 for d in dirs:
     try: 
@@ -47,15 +47,24 @@ for d in dirs:
 if len(dirlist) == 0:
     sys.exit('No time subdirectories found in '+str(dirs))
 
+# set up search strings for the expected variables with underscore vars first
+varlist = args.vars
+expectedVars = [v for v in varlist if v.__contains__('_')] \
+             + [v for v in varlist if not v.__contains__('_')]
+if verbose:
+    print('Expected variables:',expectedVars)
+
 #-----------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------
 
 # now, find sample and variable name(s)
 sampleNames = []
+fullVarNames = [] # consisting of one or more variable names separated by '_'
 varNames = []
 extNames = []
 ext = None
+nameFirst = None
 for timestep_dir in dirlist:
     if verbose:
         print('Processing', timestep_dir)
@@ -64,27 +73,38 @@ for timestep_dir in dirlist:
     for f in filelist:
         if f.startswith('.'):
             continue
-        basename,ext = os.path.splitext(f)
+        name,ext = os.path.splitext(f)
+        basename = name
         ext = ext[1:] # drop leading '.' from extension
-        origname = None
-        for uname in underscoreNames:
-            if uname in basename:
-                origname = uname
-                basename = basename.replace(uname,'TEMP')
-        basesplit = basename.split('_')
-        var = basesplit[0]
-        if origname is not None:
-            var = var.replace('TEMP',origname)
-        name = '_'.join(basesplit[1:])
+        newvars = []
+        for var in expectedVars:
+            if var in name:
+                newvars.append(var)
+                name = name.replace(var,'')
+        if name.endswith('_'):
+            nameFirst = True
+            name = name.rstrip('_')
+        elif name.startswith('_'):
+            nameFirst = False
+            name = name.lstrip('_')
+        else:
+            name = name.replace('_','')
         if verbose:
-            print('  {:s}\t(name={:s}, var={:s}, ext={:s})'.format(f,name,var,ext))
+            print('  {:s}\t(name={:s}, var={:s}, ext={:s})'.format(f,name,','.join(newvars),ext))
         if name=='':
             name = 'timeSeries'
+        else:
+            if nameFirst:
+                fullvarname = basename.replace(name+'_','')
+            else:
+                fullvarname = basename.replace('_'+name,'')
         if not name in sampleNames:
             sampleNames.append(name)
             if not os.path.exists(name): os.makedirs(name)
-        if not var in varNames:
-            varNames.append(var)
+        for var in newvars:
+            if var not in varNames:
+                varNames.append(var)
+                fullVarNames.append(fullvarname)
         if not ext in extNames:
             extNames.append(ext)
 if ext is None:
@@ -99,16 +119,18 @@ else:
 # create symlinks
 indices = sorted(range(len(timesteps)), key=lambda k: timesteps[k])
 for sample in sampleNames:
-    for var in varNames:
+    for var,fullvar in zip(varNames,fullVarNames):
         for i in range(len(timesteps)):
             idx = indices[i]
             dname = dirlist[idx]#.split()
             if sample=='timeSeries':
                 src = os.path.join( os.getcwd(), dname, var+'.'+ext )
-                dest = sample + os.sep + '%s_%s.%s' % (var,i,extNew)
             else:
-                src = os.path.join( os.getcwd(), dname, var+'_'+sample+'.'+ext )
-                dest = sample + os.sep + '%s_%s.%s' % (var,i,extNew)
+                if nameFirst:
+                    src = os.path.join( os.getcwd(), dname, sample+'_'+fullvar+'.'+ext )
+                else:
+                    src = os.path.join( os.getcwd(), dname, fullvar+'_'+sample+'.'+ext )
+            dest = os.path.join(sample, '{:s}_{:d}.{:s}'.format(var,i,extNew))
             if verbose:
                 print(dest,'-->',src)
             try:
@@ -118,5 +140,6 @@ for sample in sampleNames:
 
 print('sample names: ',sampleNames)
 print('field names: ',varNames)
+print('full field names: ',fullVarNames)
 print('time steps: ',len(timesteps),'[',timesteps[indices[0]],'...',timesteps[indices[-1]],']')
 
